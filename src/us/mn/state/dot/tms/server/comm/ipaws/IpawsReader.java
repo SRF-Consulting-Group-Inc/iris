501fe7168a22f2ba1a75cadab3d2ddb724f30560
Gordon Parikh <gparikh@srfconsulting.com>
2020-05-21 16:17:48 -0500

    Alert XMLs saved if exceptions hit, reduced line lengths

--- src/us/mn/state/dot/tms/server/comm/ipaws/IpawsReader.java
+++ src/us/mn/state/dot/tms/server/comm/ipaws/IpawsReader.java
@@ -14,13 +14,23 @@
  */
 package us.mn.state.dot.tms.server.comm.ipaws;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.nio.file.Files;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -40,38 +50,48 @@ import org.xml.sax.SAXException;
 
 import us.mn.state.dot.sched.TimeSteward;
 import us.mn.state.dot.sonar.SonarException;
+import us.mn.state.dot.tms.DMSHelper;
 import us.mn.state.dot.tms.IncidentHelper;
+import us.mn.state.dot.tms.IpawsAlert;
+import us.mn.state.dot.tms.IpawsAlertHelper;
 import us.mn.state.dot.tms.TMSException;
 import us.mn.state.dot.tms.server.IpawsAlertImpl;
 import us.mn.state.dot.tms.server.comm.ntcip.mib1204.Json;
 
 /**
-
+ * Integrated Public Alert and Warning System (IPAWS) alert reader. Reads and
+ * parses IPAWS CAP XMLs into IpawsAlert objects and saves them to the
+ * database. 
  *
  * @author Michael Janson
+ * @author Gordon Parikh
  */
 
 public class IpawsReader {
 	
-	private static final String dtFormat = "YYYY-MM-dd'T'HH:mm:ssX";
-	private static final SimpleDateFormat dtFormatter = new SimpleDateFormat(dtFormat);
+	/** Date formatters */
+	// 2020-05-12T21:59:23-00:00
+	private static final String dtFormat = "yyyy-MM-dd'T'HH:mm:ssX";
+	private static final SimpleDateFormat dtFormatter =
+			new SimpleDateFormat(dtFormat);
 	
-	
-//	/** Cache of incident proxy objects */
+//	/** Cache of IPAWS alert proxy objects */
 //	private final TypeCache<IpawsAlert> cache;
 
-    public static void readIpaws() throws IOException {
-        File folder = new File("\\\\srfgroup.loc\\Shares\\RDF\\mjanson\\Folders\\Documents\\ipaws_files");
-        //File folder = new File("\\\\srfgroup.loc\\Shares\\RDF\\mjanson\\Folders\\Documents\\srf-iris-ipaws workspace\\srf-iris-ipaws\\var\\log\\iris\\test");
+	/** Read alerts from a directory containing XML files. */
+    public static void readIpaws(String dirPath) throws IOException {
+        File folder = new File(dirPath);
 
         File[] files = folder.listFiles();
         
-        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilderFactory dbFactory =
+        		DocumentBuilderFactory.newInstance();
         DocumentBuilder dBuilder;
         try {
         	dBuilder = dbFactory.newDocumentBuilder();
 		    for (File xmlFile : files) {
-		        	InputStream in = new GZIPInputStream(new FileInputStream(xmlFile));
+		        	InputStream in = new GZIPInputStream(
+		        			new FileInputStream(xmlFile));
 		            Document doc = dBuilder.parse(in);
 		            doc.getDocumentElement().normalize();
 		
@@ -81,41 +101,92 @@ public class IpawsReader {
 			                getIpawsAlert(nodeList.item(i));
 			            }
 		            }catch(SonarException e) {
-//		        		if (e.getMessage().contains("Name already exists")) continue;
+//		        		if (e.getMessage().contains(
+//		        				"Name already exists"))
+//		        			continue;
 //		        		else throw new SonarException(e);
 		            }
 		    }
-        }catch(ParserConfigurationException | SAXException | ParseException | TMSException e) {
+        }catch(ParserConfigurationException | SAXException
+        		| ParseException | TMSException e) {
         	e.printStackTrace();
         }
     }
     
+    /** Read alerts from an InputStream (live feed). */
     public static void readIpaws(InputStream is) throws IOException {
-        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+    	// make a copy of the input stream - if we hit an exception we will
+    	// save the XML and the text of the exception on the server
+    	// TODO make these controllable with a system attribute
+    	ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    	byte[] buf = new byte[1024];
+    	int len;
+    	while ((len = is.read(buf)) > -1)
+    		baos.write(buf, 0, len);
+    	baos.flush();
+    	InputStream is1 = new ByteArrayInputStream(baos.toByteArray());
+    	
+        DocumentBuilderFactory dbFactory =
+        		DocumentBuilderFactory.newInstance();
         DocumentBuilder dBuilder;
         try {
         	dBuilder = dbFactory.newDocumentBuilder();
-	            Document doc = dBuilder.parse(is);
-	            doc.getDocumentElement().normalize();
-	
-	            NodeList nodeList = doc.getElementsByTagName("alert");
-		            for (int i = 0; i < nodeList.getLength(); i++) {
-		                getIpawsAlert(nodeList.item(i));
-		            }
-        }catch(ParserConfigurationException | SAXException | ParseException | TMSException | SonarException e) {
-        	e.printStackTrace();
+            Document doc = dBuilder.parse(is1);
+            doc.getDocumentElement().normalize();
+
+            NodeList nodeList = doc.getElementsByTagName("alert");
+	            for (int i = 0; i < nodeList.getLength(); i++) {
+	                getIpawsAlert(nodeList.item(i));
+	            }
+         } catch(ParserConfigurationException | SAXException | ParseException
+        		| TMSException | SonarException e) {
+        	System.out.println("Hit exception: " + e.getClass().getName());
+//        	e.printStackTrace();
+        	
+        	// save the XML contents to a file
+        	DateTimeFormatter dtf = DateTimeFormatter.ofPattern(
+        			"yyyyMMdd-HHmmss");
+        	String dts = dtf.format(LocalDateTime.now());
+        	String fn = String.format(
+        			"/var/log/iris/IpawsAlert_err_%s.xml", dts);
+        	OutputStream xmlos = new FileOutputStream(fn);
+        	baos.writeTo(xmlos);
+        	
+        	// and the exception too (ipaws_alert_<date>_exc.log)
+        	String excfn = String.format(
+        			"/var/log/iris/IpawsAlert_err_%s_exc.log", dts);
+        	PrintWriter pw = new PrintWriter(new FileWriter(excfn));
+        	e.printStackTrace(pw);
+        	System.out.println(String.format("See %s for details", excfn));
         }
     }
     
-    private static void getIpawsAlert(Node node) throws ParseException, SonarException, TMSException {
+    private static void getIpawsAlert(Node node)
+    		throws ParseException, SonarException, TMSException {
 
+//    	System.out.println("In IpawsReader.getIpawsAlert");
         if (node.getNodeType() == Node.ELEMENT_NODE) {
             Element element = (Element) node;
-
-            IpawsAlertImpl ia = new IpawsAlertImpl(getTagValue("identifier", element));
+            
+            // check if the alert exists
+            String n = getTagValue("identifier", element);
+            String name = n;
+            IpawsAlertImpl ia =
+            		(IpawsAlertImpl) IpawsAlertHelper.lookup(name);
+            
+            // if it doesn't, create a new one
+            boolean newObj = false;
+            if (ia == null) {
+            	System.out.println("Creating new alert with name: " + name);
+            	ia = new IpawsAlertImpl(name);
+            	newObj = true;
+            } else
+            	System.out.println("Updating alert with name: " + name);
+            
+            // either way set all the values
             ia.setIdentifier(getTagValue("identifier", element));
             ia.setSender(getTagValue("sender", element));
-            ia.setSentDate(checkDate(getTagValue("sent", element)));
+            ia.setSentDate(parseDate(getTagValue("sent", element)));
             ia.setStatus(getTagValue("status", element));
             ia.setMsgType(getTagValue("msgType", element));
             ia.setScope(getTagValue("scope", element));
@@ -130,9 +201,9 @@ public class IpawsReader {
             ia.setSeverity(getTagValue("severity", element));
             ia.setCertainty(getTagValue("certainty", element));
             ia.setAudience(getTagValue("audience", element));
-            ia.setEffectiveDate(checkDate(getTagValue("effective", element)));
-            ia.setOnsetDate(checkDate(getTagValue("onset", element)));
-            ia.setExpirationDate(checkDate(getTagValue("expires", element)));
+            ia.setEffectiveDate(parseDate(getTagValue("effective", element)));
+            ia.setOnsetDate(parseDate(getTagValue("onset", element)));
+            ia.setExpirationDate(parseDate(getTagValue("expires", element)));
             ia.setSenderName(getTagValue("senderName", element));
             ia.setHeadline(getTagValue("headline", element));
             ia.setAlertDescription(getTagValue("description", element));
@@ -141,15 +212,18 @@ public class IpawsReader {
             ia.setParameters(getValuePairJson("parameter", element));
             ia.setArea(getAreaJson("area", element));
             
-            ia.notifyCreate();
+            if (newObj) {
+            	System.out.println("Creating alert object");
+            	ia.notifyCreate();
+            }
         }
 
     }
     
-    private static Date checkDate(String dte) {
-    	try{
+    private static Date parseDate(String dte) {
+    	try {
     		return dtFormatter.parse(dte);
-    	}catch(ParseException | NullPointerException e) {
+    	} catch(ParseException | NullPointerException e) {
     		return null;
     	}
     }  		
@@ -187,8 +261,10 @@ public class IpawsReader {
 	        NodeList nodeList = element.getElementsByTagName(tag);
 	        for (int i = 0; i < nodeList.getLength(); i++) {
 	            Element childElement = (Element)  nodeList.item(i);
-	        	String key = childElement.getElementsByTagName("valueName").item(0).getTextContent();
-	        	String value = childElement.getElementsByTagName("value").item(0).getTextContent();
+	        	String key = childElement.getElementsByTagName("valueName")
+	        			.item(0).getTextContent();
+	        	String value = childElement.getElementsByTagName("value")
+	        			.item(0).getTextContent();
 	        	sb.append(Json.str(key, value));
 	        }
         }
@@ -212,7 +288,8 @@ public class IpawsReader {
 	        for (int i = 0; i < areaNodeList.getLength(); i++) {
 	            Element areaElement = (Element)  areaNodeList.item(i);
 	            
-	            String [] element_list = {"areaDesc","polygon","circle","geocode","altitude","ceiling"};
+	            String [] element_list = {"areaDesc","polygon",
+	            		"circle","geocode","altitude","ceiling"};
 	            for (String child_element: element_list)
 	            	appendElementJson(child_element, areaElement, sb);
 	        }
@@ -226,9 +303,11 @@ public class IpawsReader {
     }
 
     
-    private static void appendElementJson(String tag, Element element, StringBuilder sb) {
+    private static void appendElementJson(String tag,
+    		Element element, StringBuilder sb) {
         if (element.getElementsByTagName(tag).getLength() == 1)      
-        	sb.append(Json.str(tag, element.getElementsByTagName(tag).item(0).getTextContent()));
+        	sb.append(Json.str(tag, element.getElementsByTagName(tag)
+        			.item(0).getTextContent()));
         else if (element.getElementsByTagName(tag).getLength()  > 1)
         	sb.append(Json.sub(tag, getValuePairJson(tag, element)));
     }
