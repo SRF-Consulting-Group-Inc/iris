29c11136b573aea5b6153df655527fc665655147
Gordon Parikh <gparikh@srfconsulting.com>
2020-05-22 18:38:25 -0500

    Added IPAWS alert event table

--- sql/migrate-ipaws.sql
+++ sql/migrate-ipaws.sql
@@ -15,3 +15,41 @@ CREATE VIEW comm_link_view AS
         JOIN iris.comm_protocol cp ON cl.protocol = cp.id;
 GRANT SELECT ON comm_link_view TO PUBLIC;
 
+-- IPAWS Alert Event table
+CREATE TABLE event.ipaws
+(
+    name text COLLATE pg_catalog."default",
+    identifier text COLLATE pg_catalog."default",
+    sender text COLLATE pg_catalog."default",
+    sent_date timestamp with time zone,
+    status text COLLATE pg_catalog."default",
+    message_type text COLLATE pg_catalog."default",
+    scope text COLLATE pg_catalog."default",
+    codes text[] COLLATE pg_catalog."default",
+    note text COLLATE pg_catalog."default",
+    alert_references text[] COLLATE pg_catalog."default",
+    incidents text[] COLLATE pg_catalog."default",
+    categories text[] COLLATE pg_catalog."default",
+    event text COLLATE pg_catalog."default",
+    response_types text[] COLLATE pg_catalog."default",
+    urgency text COLLATE pg_catalog."default",
+    severity text COLLATE pg_catalog."default",
+    certainty text COLLATE pg_catalog."default",
+    audience text COLLATE pg_catalog."default",
+    effective_date timestamp with time zone,
+    onset_date timestamp with time zone,
+    expiration_date timestamp with time zone,
+    sender_name text COLLATE pg_catalog."default",
+    headline text COLLATE pg_catalog."default",
+    alert_description text COLLATE pg_catalog."default",
+    instruction text COLLATE pg_catalog."default",
+    parameters jsonb,
+    area jsonb,
+	purgeable boolean DEFAULT false
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE event.ipaws
+    OWNER to tms;
+
