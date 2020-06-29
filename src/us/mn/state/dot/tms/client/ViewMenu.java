bbd7c3def124281bf44e7cd25120d6804f1b4e6f
Gordon Parikh <gparikh@srfconsulting.com>
2020-06-29 18:00:27 -0500

    Removed unused camera_test package

--- src/us/mn/state/dot/tms/client/ViewMenu.java
+++ src/us/mn/state/dot/tms/client/ViewMenu.java
@@ -29,11 +29,9 @@ import us.mn.state.dot.tms.client.lcs.LaneUseMenu;
 import us.mn.state.dot.tms.client.reports.RptSetupForm;
 import us.mn.state.dot.tms.client.schedule.ScheduleForm;
 import us.mn.state.dot.tms.client.system.SystemMenu;
-import us.mn.state.dot.tms.client.weather.WeatherSensorForm;
 import us.mn.state.dot.tms.client.widget.IAction;
 import us.mn.state.dot.tms.client.widget.IMenu;
 import us.mn.state.dot.tms.client.widget.SmartDesktop;
-import us.mn.state.dot.tms.client.camera_test.CameraTest;
 
 /**
  * ViewMenu is a JMenu which contains items to view various TMS object types.
