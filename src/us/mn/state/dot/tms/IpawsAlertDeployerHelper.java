a1e2ce15e7b1e3f9933e1c8d5339f4953ca1982c
Gordon Parikh <gparikh@srfconsulting.com>
2020-08-13 18:01:52 -0500

    Added methods to allow for multiple deployers per alert

--- src/us/mn/state/dot/tms/IpawsAlertDeployerHelper.java
+++ src/us/mn/state/dot/tms/IpawsAlertDeployerHelper.java
@@ -15,6 +15,9 @@
 
 package us.mn.state.dot.tms;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.Iterator;
 
 /**
@@ -35,16 +38,71 @@ public class IpawsAlertDeployerHelper extends BaseHelper {
 				IpawsAlertDeployer.SONAR_TYPE, name);
 	}
 
-	/** Lookup an alert deployer object for the specified IpawsAlert name. */
-	static public IpawsAlertDeployer lookupAlertDeployerName(String alertId) {
+	/** Lookup an alert deployer object for the specified IpawsAlert name.
+	 *  Returns the most recent deployer for this alert.
+	 */
+	static public IpawsAlertDeployer lookupDeployerFromAlert(String alertId) {
+		// get the list of deployers for this alert sorted newest to oldest
+		ArrayList<IpawsAlertDeployer> deployers = getDeployerList(alertId);
+		if (deployers.size() > 0)
+			return deployers.get(0);
+		return null;
+	}
+	
+	/** Comparator to sort IpawsAlertDeployers by genTime. */
+	static private class DeployerGenTimeComparator
+				implements Comparator<IpawsAlertDeployer> {
+		
+		/** Multiplier to sort ascending (+1) or descending (-1) */
+		private int ascMult = 1;
+		
+		public DeployerGenTimeComparator(boolean ascending) {
+			if (!ascending)
+				ascMult = -1;
+		}
+		
+		@Override
+		public int compare(IpawsAlertDeployer o1, IpawsAlertDeployer o2) {
+			// get genTimes
+			Date gt0 = o1.getGenTime();
+			Date gt1 = o2.getGenTime();
+			
+			// check for nulls (just in case) - the non-null one should
+			// be higher
+			if (gt0 == null && gt1 != null)
+				return 1;
+			else if (gt0 != null && gt1 == null)
+				return -1;
+			else if (gt0 == null && gt1 == null)
+				return 0;
+			
+			// no nulls - compare dates and apply multiplier
+			return ascMult * gt0.compareTo(gt1);
+		}
+		
+	}
+	
+	/** Get a list of all deployers associated with the alert ID provided.
+	 *  Objects are sorted from newest to oldest.
+	 */
+	static public ArrayList<IpawsAlertDeployer>
+					getDeployerList(String alertId) {
+		ArrayList<IpawsAlertDeployer> deployers =
+				new ArrayList<IpawsAlertDeployer>();
+		
+		// find all deployers associated with this alert
 		Iterator<IpawsAlertDeployer> it = iterator();
 		while (it.hasNext()) {
 			IpawsAlertDeployer ian = it.next();
 			if (ian.getAlertId().equals(alertId))
-				return ian;
+				deployers.add(ian);
 		}
-		return null;
+		
+		// sort the list using a custom comparator
+		deployers.sort(new DeployerGenTimeComparator(false));
+		return deployers;
 	}
+	
 	
 	/** Get an IpawsAlertDeployer object iterator */
 	static public Iterator<IpawsAlertDeployer> iterator() {
