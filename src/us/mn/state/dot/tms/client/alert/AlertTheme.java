789a5455d03775fabb1d7e0cd5764f4b4272bf93
Gordon Parikh <gparikh@srfconsulting.com>
2020-08-28 16:39:44 -0500

    Started alert area drawing method (need to actually draw it)

--- src/us/mn/state/dot/tms/client/alert/AlertTheme.java
+++ src/us/mn/state/dot/tms/client/alert/AlertTheme.java
@@ -15,10 +15,18 @@
 package us.mn.state.dot.tms.client.alert;
 
 import java.awt.Color;
+import java.awt.Graphics2D;
 
+import org.postgis.MultiPolygon;
+
+import us.mn.state.dot.tms.GeoLoc;
+import us.mn.state.dot.tms.IpawsAlert;
 import us.mn.state.dot.tms.IpawsAlertDeployer;
+import us.mn.state.dot.tms.IpawsAlertHelper;
 import us.mn.state.dot.tms.ItemStyle;
 import us.mn.state.dot.tms.client.incident.IncidentMarker;
+import us.mn.state.dot.tms.client.map.MapObject;
+import us.mn.state.dot.tms.client.proxy.MapGeoLoc;
 import us.mn.state.dot.tms.client.proxy.ProxyManager;
 import us.mn.state.dot.tms.client.proxy.ProxyTheme;
 
@@ -36,4 +44,37 @@ public class AlertTheme extends ProxyTheme<IpawsAlertDeployer> {
 		addStyle(ItemStyle.ALL, Color.WHITE);
 	}
 	
+	/** Draw the specified map object. Overridden to do nothing so only
+	 *  selected alerts are drawn.
+	 *  
+	 *  TODO decide if we want to draw non-selected alerts too and distinguish
+	 *  selected ones somehow...
+	 */
+	@Override
+	public void draw(Graphics2D g, MapObject mo) { }
+	
+	/** Draw the alert on the map. The GeoLoc is the centroid of the alert and
+	 *  is not drawn (really just kind of a hack to get this method to fire).
+	 *  The alert area is drawn, along with DMS selected for the alert.
+	 */
+	@Override
+	public void drawSelected(Graphics2D g, MapObject mo) {
+		if (mo instanceof MapGeoLoc) {
+			// get the GeoLoc from the object and use it to lookup the alert
+			MapGeoLoc mgl = (MapGeoLoc) mo;
+			GeoLoc gl = mgl.getGeoLoc();
+			IpawsAlert ia = IpawsAlertHelper.lookup(gl.getName());
+			
+			// draw the alert area
+			// TODO need to make sure the PostGIS JAR makes it to the client
+			// (and actually works) in WebStart
+			MultiPolygon mp = ia.getGeoPoly();
+			System.out.println("Drawing polygon: " + mp.toString());
+			
+			// TODO draw the polygon (need to pull out coordinates and draw
+			// the polygon as a path, making sure to work in the right
+			// coordinate system (maybe WGS will be OK, probably need to do 
+			// some transformation(s))
+		}
+	}
 }
